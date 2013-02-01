8a3520db52c03e3d4a973354db51f976d6285ddb
Patric Fornasier <patric.fornasier@gmail.com>
2013-02-01 19:59:16 +0000

    pat: fetchin fb user details

--- ambient/AppDelegate.m
+++ ambient/AppDelegate.m
@@ -1,20 +1,15 @@
 #import "AppDelegate.h"
 #import <FacebookSDK/FacebookSDK.h>
 
-@interface AppDelegate()
-- (BOOL) isLoggedIn;
-@end
-
 @implementation AppDelegate
 
 #pragma mark UIApplicationDelegate
 
 - (BOOL) application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [application setNetworkActivityIndicatorVisible:true];
     
     if ([self isLoggedIn]) {
-        NSLog(@"Already logged in");
-        [self handleLoginSuccess];
+        NSLog(@"Already logged in.");
+        [self loginUsingFacebook];
     }
     return YES;
 }
@@ -30,28 +25,29 @@
     [FBSession.activeSession handleDidBecomeActive];
 }
 
-#pragma mark Facebook Login
+#pragma mark FB Login
 
 - (void) sessionStateChanged:(FBSession *)session state:(FBSessionState) state error:(NSError *)error {
     
     switch (state) {
         case FBSessionStateOpen: {
-            NSLog(@"Login successful.");
+            NSLog(@"FB Session opened.");
             [self handleLoginSuccess];
         } break;
         case FBSessionStateClosed:
-            NSLog(@"Session closed.");
+            NSLog(@"FB Session closed.");
             break;
         case FBSessionStateClosedLoginFailed:
-            NSLog(@"Login failed.");
-            [FBSession.activeSession closeAndClearTokenInformation];            
+            NSLog(@"FB Login failed.");
+            [self handleLoginFailure];
             break;
         default:
             break;
     }
     
     if (error) {
-        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error" message:error.localizedDescription delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [self showLoginScreen];
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Oops..." message:error.localizedDescription delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
         [alertView show];        
     }
 }
@@ -62,15 +58,48 @@
     }];
 }
 
-- (void) handleLoginSuccess {    
-    UINavigationController *navigationController = (UINavigationController*)self.window.rootViewController;
-    [navigationController.topViewController performSegueWithIdentifier:@"NearbySegue" sender:self];
-}
-
-#pragma mark private
-
 - (BOOL) isLoggedIn {
     return FBSession.activeSession.state == FBSessionStateCreatedTokenLoaded;
+}
+
+- (void) handleLoginSuccess {
+    [self fetchUserDetails]; // FIXME move
+    [self segueTo:@"NearbySegue"];
+}
+
+- (void) handleLoginFailure {
+    [FBSession.activeSession closeAndClearTokenInformation];
+    [self showLoginScreen];
+}
+
+- (void) showLoginScreen {
+    UINavigationController *navigationController = (UINavigationController*)self.window.rootViewController;
+    [navigationController popToRootViewControllerAnimated:false];
+}
+
+- (void) segueTo:(NSString *)segueId {
+    UINavigationController *navigationController = (UINavigationController*)self.window.rootViewController;
+    [navigationController.topViewController performSegueWithIdentifier:segueId sender:self];
+}
+
+
+#pragma mark FB personalisation
+
+- (void) fetchUserDetails {
+    if (FBSession.activeSession.isOpen) {
+        NSLog(@"About to fetch user details...");
+        [[FBRequest requestForMe] startWithCompletionHandler:^(FBRequestConnection *connection, NSDictionary<FBGraphUser> *user, NSError *error) {
+             if (!error) {
+                NSLog(@"user.firstName = %@, user.lastName = %@, user.id = %@", user.first_name, user.last_name, user.id);
+             } else {
+                NSLog(@"Error retrieving user details: %@", error.localizedDescription);
+                [self handleLoginFailure];
+             }
+         }];
+    } else {
+        NSLog(@"No active open session =(");
+        [self handleLoginFailure];        
+    }
 }
 
 @end
